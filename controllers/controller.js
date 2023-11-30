e041f53db73c360db2d9227408e3d80daeeea5ae
Dystrima <138579225+Dystrima@users.noreply.github.com>
2023-11-30 16:47:40 +0100

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,57 +1,61 @@
 // Dashboard page
 module.exports.dashboardGet = (req, res) => {
-    console.log("Ici, c'est la Dashboard page");
-    res.render("index");
+	console.log("Ici, c'est la Dashboard page");
+	res.render("index");
 };
 
 // Login page
 module.exports.loginGet = (req, res) => {
-    console.log("Ici, c'est la Login page");
-    res.render("login");
+	console.log("Ici, c'est la Login page");
+	res.render("login");
 };
 
 module.exports.loginPost = (req, res) => {
-    res.send("Login envoyé");
+	console.log("Login envoyé");
+	res.send("Login envoyé");
 };
 
 // Register page
 module.exports.registerGet = (req, res) => {
-    console.log("Ici, c'est la Register page");
-    res.render("register");
+	console.log("Ici, c'est la Register page");
+	res.render("register");
 };
 
 module.exports.registerPost = (req, res) => {
-    res.send("Register envoyé");
+	console.log("Register envoyé");
+	res.send("Register envoyé");
 };
 
 // Logout page
 module.exports.logoutGet = (req, res) => {
-    console.log("Ici, c'est la Logout page");
-    res.render("login");
+	console.log("Ici, c'est la Logout page");
+	res.render("login");
 };
 
 // Profile page
 module.exports.profileGet = (req, res) => {
-    console.log("Ici, c'est la Profile page");
-    res.render("profile");
+	console.log("Ici, c'est la Profile page");
+	res.render("profile");
 };
 
 // Create a job page
 module.exports.createOfferGet = (req, res) => {
-    console.log("Ici, c'est la Create an offer page");
-    res.render("create");
+	console.log("Ici, c'est la Create an offer page");
+	res.render("create");
 };
 
 module.exports.createOfferPost = (req, res) => {
-    res.send("Nouvelle offre envoyée");
+	console.log("Nouvelle offre envoyée");
+	res.send("Nouvelle offre envoyée");
 };
 
 // Edit a job page
 module.exports.editOfferGet = (req, res) => {
-    console.log("Ici, c'est la Edit an offer page");
-    res.render("update");
+	console.log("Ici, c'est la Edit an offer page");
+	res.render("update");
 };
 
 module.exports.editOfferPut = (req, res) => {
-    res.send("Offre modifiée");
+	console.log("Offre modifiée");
+	res.send("Offre modifiée");
 };
