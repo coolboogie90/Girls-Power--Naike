0fac2e37ce80d9c1e83de4741197352071be17fe
JuliaPasquier <pasquierjulia5@gmail.com>
2023-11-30 14:57:01 +0100

    created users

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+
 require("dotenv").config();
 
 const express = require("express");
 const router = require("./routes/router");
 const app = express();
+const mongoose = require('mongoose');
+
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,11 +16,17 @@ app.use(express.json());
 /* View engine */
 app.set("view engine", "ejs");
 
-/* Lauch server */
-app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}`);
-});
 
 /* Routes */
 // app.get("*", checkUser);
 app.use(router);
+
+
+// database connection
+const dbURI = process.env.DB_URI;
+
+mongoose.connect(dbURI)
+  .then(() => app.listen(PORT, () => {
+	console.log(`App listening on port ${PORT}`);
+}))
+  .catch((err) => console.log(err));
