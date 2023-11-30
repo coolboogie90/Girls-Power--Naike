f3ba31c5aaa47c8ef2c851b25cd1f0f3ab3ebbbb
Dystrima <138579225+Dystrima@users.noreply.github.com>
2023-11-30 13:40:36 +0100

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -4,19 +4,25 @@ const controller = require("../controllers/controller");
 
 // Dashboard
 router.get("/", controller.dashboardGet);
+
 // Login
 router.get("/login", controller.loginGet);
 router.post("login", controller.loginPost);
+
 // Register
 router.get("/register", controller.registerGet);
 router.post("/register", controller.registerPost);
+
 // Logout
 router.get("/logout", controller.logoutGet);
+
 // Profile
 router.get("/profile", controller.profileGet);
+
 // Create a job
 router.get("/create-offer", controller.createOfferGet);
 router.post("/create-offer", controller.createOfferPost);
+
 // Edit a job
 router.get("/edit-offer", controller.editOfferGet);
 router.put("/edit-offer", controller.editOfferPut);
