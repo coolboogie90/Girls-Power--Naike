899a71b00f5cbdfc98f20f4fef1ce3d38aceb59e
Naïké Dramé <naike.drame@gmail.com>
2023-12-01 10:12:19 +0100

    added vercel js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-
 require("dotenv").config();
 
 const express = require("express");
 const router = require("./routes/router");
 const app = express();
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,17 +14,20 @@ app.use(express.json());
 /* View engine */
 app.set("view engine", "ejs");
 
-
 /* Routes */
 // app.get("*", checkUser);
 app.use(router);
 
-
 // database connection
 const dbURI = process.env.DB_URI;
 
-mongoose.connect(dbURI)
-  .then(() => app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}`);
-}))
-  .catch((err) => console.log(err));
+mongoose
+    .connect(dbURI)
+    .then(() =>
+        app.listen(PORT, () => {
+            console.log(`App listening on port ${PORT}`);
+        })
+    )
+    .catch((err) => console.log(err));
+
+module.exports = app;
