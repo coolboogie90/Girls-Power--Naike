a28b93479835be4783c593cdfb84acdc4ca3e7c9
Dystrima <138579225+Dystrima@users.noreply.github.com>
2023-12-11 10:10:44 +0100

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,6 +6,13 @@ import { v2 as cloudinary } from "cloudinary";
 
 let currentUserId;
 
+// Cloudinary config
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 // Handle errors
 const handleErrors = (err) => {
 	console.log(err.message, err.code);
